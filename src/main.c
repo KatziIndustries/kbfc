968068bf9cf6b5985e8d5a0d5efdac4631cb4b2d
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-08-25 18:48:03 +0000

    planning the compiler/transformer

--- src/main.c
+++ src/main.c
@@ -5,6 +5,15 @@ int main(int argc, char *argv[])
     if(argc == 1) {
         printf("Usage: %s <file>\n",argv[0]);
     }else {
+        //argv
+        printf("Extracting argv!\n");
+        //file_check
         printf("Finding files!\n");
+        //transforming
+        printf("Converting files!\n");
+        //linking
+        printf("Combinding files!\n");
+        //create_output_file
+        printf("Creating Result!\n");
     }
 }
